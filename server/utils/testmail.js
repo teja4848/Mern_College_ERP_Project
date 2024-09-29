9e4ad11aa0456e680513439736990a3fac302ff6
teja4848 <tejaanumolu70@gmail.com>
2024-09-30 04:05:36 +0530

    Final commit -  ERP project files

--- server/utils/testmail.js
+++ server/utils/testmail.js
@@ -6,14 +6,14 @@ async function sendEmail() {
         port: 465,
         secure: true,
         auth: {
-            user: 'tejaanumolu70@gmail.com', // your Gmail address
-            pass: 'nfoc cxgb dnws llxh'      // your App Password or Gmail password
+            user: 'sender-mail', // your Gmail address
+            pass: 'app pass'      // your App Password or Gmail password
         }
     });
 
     let info = await transporter.sendMail({
-        from: '"teja" <tejaanumolu70@gmail.com>',
-        to: 'tejaanumolu16@gmail.com', // list of receivers
+        from: '"user" <sender-mail>',
+        to: 'reciever-mail', // list of receivers
         subject: 'Hello ✔', // Subject line
         text: 'Hello world?', // plain text body
         html: '<b>Hello world?</b>' // html body
